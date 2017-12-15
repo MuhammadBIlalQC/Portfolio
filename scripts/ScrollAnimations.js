06e202105eb3fd523c52db7c2da735e359e35b30
Muhammad Bilal <MuhammadBilalQC@hotmail.com>
2017-12-15 00:24:22 -0500

    added animation to main paragraph

---
+++ scripts/ScrollAnimations.js
@@ -0,0 +1,36 @@
+function inWindowView(elementID)
+{
+    var elem = $('#' + elementID + '-container');
+    var windowTop = window.pageYOffset;
+    var windowBot = windowTop + window.innerHeight;
+    var elemTop = elem.position().top;
+    var elemBot = elem.height() + elemTop;
+    if (elemTop > windowTop && elemTop < windowBot)
+        return true;
+    
+    return false;
+}
+
+function showAboutMe()
+{
+    if(inWindowView('about-me'))
+    {
+        $(window).off('scroll', showAboutMe);
+        $('#about-me').slideDown(3000);
+    }
+}
+
+function showCat()
+{
+    if (inWindowView('cat'))
+    {
+        $(window).off('scroll', showCat);
+        $('#cat').slideToggle(2000);
+    }
+}
+
+$(document).ready(function(){
+    $(window).on('scroll', showAboutMe);
+    //$(window).on('scroll', showCat);
+});
+
